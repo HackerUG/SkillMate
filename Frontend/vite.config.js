78d956c643c83d449625e7b40175e0a15d83e1d4
HackerUG <Utkarsh3663@gmail.com>
2025-10-26 02:14:51 +0530

    Add redirects for SPA routing

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
 
 export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
   },
+  publicDir: 'public',
   server: {
     proxy: {
       "/api": "http://localhost:5000"
