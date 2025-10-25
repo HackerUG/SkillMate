4cdca9497e6bbd093453c4cfc5dc393aee788660
HackerUG <Utkarsh3663@gmail.com>
2025-10-26 04:31:45 +0530

    my profile pic

--- Frontend/src/Components/About.jsx
+++ Frontend/src/Components/About.jsx
@@ -150,7 +150,7 @@ const About = () => {
           className="creator-card"
         >
           <img
-            src="src/assets/hndsome.jpg"
+            src="src/assets/creator.jpg"
             alt="Creator"
             className="creator-img"
           />
