8c10ee3517d32103b32cd989b43ecae4bec93a84
HackerUG <Utkarsh3663@gmail.com>
2025-10-26 02:57:34 +0530

    destination and vite file change

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,18 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-
 
 export default defineConfig({
-  base: '/',
+  plugins: [react()],
   build: {
     outDir: 'dist',
   },
-  publicDir: 'public',
   server: {
     proxy: {
-      "/api": "http://localhost:5000"
-    }
+      '/api': 'http://localhost:5000',
+    },
   },
-  plugins: [react()],
 })
